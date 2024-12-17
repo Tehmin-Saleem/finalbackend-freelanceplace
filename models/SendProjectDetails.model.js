1622a85fc5da5d7e38b97e277be26129bc3876b0
Sammar Zahra <zahrasamar88@gmail.com>
2024-12-17 16:17:20 +0500

    Feat:consultant work start

---
+++ models/SendProjectDetails.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const ConsultantOfferSchema = new mongoose.Schema({
+  consultantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Consultant' },
+  clientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' },
+  status: { type: String, default: 'Pending' },
+  projectDetails: {
+    
+    deadline: Date,
+    githubUrl: String,
+    additionalNotes: String,
+  },
+});
+
+module.exports = mongoose.model('ConsultantOffer', ConsultantOfferSchema);
