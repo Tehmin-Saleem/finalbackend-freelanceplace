6a2904cb300e94a49f219366cc9350caff814b6e
laiba mir <laibabasharat0987@gmail.com>
2025-01-14 20:11:01 -0800

    feat: corrected flow for reviews for offers

--- src/Pages/FreelancerDashboard/index.jsx
+++ src/Pages/FreelancerDashboard/index.jsx
@@ -138,6 +138,7 @@ const FreelanceDashboardPage = () => {
           Authorization: `Bearer ${token}`,
         },
       };
+      const BASE_URL = import.meta.env.VITE_LOCAL_BASE_URL
 
       const response = await axios.get(
         `${BASE_URL}/api/freelancer/freelancer-profile-exists/${userId}`,
@@ -212,6 +213,7 @@ const FreelanceDashboardPage = () => {
 
         // Check profile existence
         await checkProfileExists(userId, token);
+        const BASE_URL = import.meta.env.VITE_LOCAL_BASE_URL
 
         const response = await axios.get(
           `${BASE_URL}/api/client/users/${userId}`,
